8cd7a46072b4e688204339d18452b046182e35f8
aybolek27 <aybolek27@gmail.com>
2018-09-30 12:57:28 -0500

    new java file added

---
+++ src/com/cbt/SeleniumDemo2.java
@@ -0,0 +1,14 @@
+package com.cbt;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumDemo2 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Aybolek\\Documents\\selenium dependencies\\drivers\\chromedriver.exe");
+
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://google.com");
+	}
+}
